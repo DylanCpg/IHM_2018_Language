356112947c8146c6d99d0af5745a27a2735dd195
dcampergue <dcampergue@scinfe062.univ-pau.fr>
2018-11-28 09:19:57 +0100

    Suite 1 du projet

---
+++ src/application/Mainapp.java
@@ -0,0 +1,44 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Mainapp extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Mainapp.class.getResource("view/Accueil.fxml"));
+            AnchorPane rootLayout = loader.load();
+            
+			Scene scene = new Scene(rootLayout,900,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			/*Button but = (Button) scene.lookup("Button");  // cherche un bouton
+			if(but != null) {  // si le bouton est trouvé
+				but.setOnAction(new EventHandler<ActionEvent>() {
+					
+					@Override
+					public void handle(ActionEvent event) { // récuper la valeur 
+						System.out.println("Clicke !!!");
+						
+					}
+				});
+			}*/
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
