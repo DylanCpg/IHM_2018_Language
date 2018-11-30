bcd132ce6698bbd5b5911f6581f707243e2f7e3f
dcampergue <dcampergue@scinfe2182.univ-pau.fr>
2018-11-30 17:06:39 +0100

    Modele Atelier Exercice et Accueil

--- src/application/Mainapp.java
+++ src/application/Mainapp.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
@@ -15,12 +16,14 @@ public class Mainapp extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Mainapp.class.getResource("view/Accueil.fxml"));
+            loader.setLocation(Mainapp.class.getResource("view/Atelier.fxml"));
             AnchorPane rootLayout = loader.load();
             
 			Scene scene = new Scene(rootLayout,900,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Accueil");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 			/*Button but = (Button) scene.lookup("Button");  // cherche un bouton
 			if(but != null) {  // si le bouton est trouvé
